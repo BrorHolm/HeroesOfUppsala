1c37c43d02220bc76faa8f0a7ff8761eae555a83
Mikael Holm <me@mikaelholm.com>
2016-05-25 15:14:55 +0200

    flyttade över det i html/index.html till index.php

--- index.php
+++ index.php
@@ -6,11 +6,50 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <title>TODO supply a title</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="html/css/style.css">
     </head>
     <body>
+        <div id="nav">    
+            <div class="section group">
+                <div class="col span_3_of_3">
+                    <ul>
+                        <li><a href="index.html">Start</a></li>
+                        <li><a href="mission.html">Missions</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>     
+        <div class="section group">
+            <div class="col span_1_of_3" id="leftnav">
+                <img src="html/img/heroesofuppsala.png" alt="Heroes of Uppsala">
+            </div>
+            <div class="col span_2_of_3" id="content">
+                
+                
+                <div class="section group">
+                    <div class="col span_3_of_3">
+                        <h1>Quick introduction</h1>
+                        <p>Do you sometimes feel like the weekday becomes a bit dull? That you would like new perspectives, friends and abilities? That you would like to make a difference? Fear no more, because now you have the chance to save the day!</p>
 
-        <p>Let's go</p>
+                        <p>Heroes of Uppsala provides the possibility to make nonprofit missions and nonprofit volunteers to meet, creating amazing connections. Just by register an account you will have access to possibilities you could only dream of.</p>
+
+                        <p>You are just minutes away from  entering a new world!</p>
+
+                    </div>
+                </div>    
+                <div class="section group">
+                    <div class="col span_3_of_3">
+                        <h1>What we are all about</h1>
+                        <p>We think that it should be easy to meet new people that are dedicated to what they believe in. Creating new things, gaining experience and connecting with people should not be more than a couple of clicks away. </p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     </body>
 </html>
